9565ff6c71b028e5ef621ced20e61e185e83d45f
tovagova <eh.vango@gmail.com>
2023-01-29 21:37:41 -0500

    Module 19 homework
    unfinished

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log("index is loaded baby");
+
+$('.breakBar').on("click", function() {
+    console.log("you clika the guy");
+});
+
+$('.projectTile').scroll(function() {
+    console.log("scrolly");
+});
+
+$('#aboutpic').on("click", function() {
+    console.log("clicking pfp");
+});
