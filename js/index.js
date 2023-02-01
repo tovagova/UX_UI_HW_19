ca4fdae28f05388950619804cbd00f3cb2d3c210
tovagova <eh.vango@gmail.com>
2023-02-01 17:38:58 -0500

    Module 19 hw

--- js/index.js
+++ js/index.js
@@ -4,10 +4,23 @@ $('.breakBar').on("click", function() {
     console.log("you clika the guy");
 });
 
-$('.projectTile').scroll(function() {
+$(window).scroll(function() {
     console.log("scrolly");
 });
 
 $('#aboutpic').on("click", function() {
     console.log("clicking pfp");
+});
+
+$(function() {
+    var bar1 = $(".breakBar");
+    var bar1height = bar1.offset().top;
+    var windowHeight = $(window).height();
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if ( scroll >= ( bar1height - windowHeight )) {
+            bar1.addClass("animEnter");
+        }
+    });
 });
